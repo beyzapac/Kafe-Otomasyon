2a77b24aaf3e9afc010d37aff9649ab6571921a3
beyzapac <beyzapac@gmail.com>
2020-06-13 15:18:28 +0300

    KafeOtomasyonu
    algo2 odev

---
+++ login.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 09 Haz 2020, 08:08:03
+-- Sunucu sürümü: 10.4.11-MariaDB
+-- PHP Sürümü: 7.2.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `login`
+--
+
+CREATE TABLE `login` (
+  `Username` text NOT NULL,
+  `Password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Tablo döküm verisi `login`
+--
+
+INSERT INTO `login` (`Username`, `Password`) VALUES
+('garson1', '1234'),
+('garson3', '1234'),
+('ornek1', '1111');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
